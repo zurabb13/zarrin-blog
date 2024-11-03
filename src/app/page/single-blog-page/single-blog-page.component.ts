9e36cf3f5a52bbed7f45f2c0709ec632d4316fae
zurabb13 <zura.baramia98@outlook.com>
2024-11-03 13:33:09 +0100

    create components

---
+++ src/app/page/single-blog-page/single-blog-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-single-blog-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './single-blog-page.component.html',
+  styleUrl: './single-blog-page.component.scss'
+})
+export class SingleBlogPageComponent {
+
+}
