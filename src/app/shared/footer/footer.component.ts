bf326b860cce1797b9011994a6f0826dacc6fca0
zurabb13 <zura.baramia98@outlook.com>
2024-11-03 14:30:08 +0100

    footer

--- src/app/shared/footer/footer.component.ts
+++ src/app/shared/footer/footer.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
 
+  date = new Date(
+
+  )
 }
